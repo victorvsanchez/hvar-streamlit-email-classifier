2263cb11b2d02843e82d50f9417263c05f238a31
Victor Viola <victor.viola@hvarconsulting.com.br>
2023-09-18 14:29:49 +0200

    Added basic outline of first page

--- Classifier.py
+++ Classifier.py
@@ -6,3 +6,30 @@ with st.sidebar:
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+
+st.title("📧 Copel Email Classifier") 
+
+uploaded_file = st.file_uploader("Faça upload de um anexo", type=("txt", "md")) 
+
+question = st.text_input(
+    "Escreva abaixo o corpo do e-mail",
+    disabled=not uploaded_file,
+)
+
+if uploaded_file and question and not openai_api_key:
+    st.info("Adicione sua key do Open AI para continuar:")
+
+if uploaded_file and question and openai_api_key:
+    #article = uploaded_file.read().decode()
+    #prompt = f"""{anthropic.HUMAN_PROMPT} Here's an article:\n\n
+    #{article}\n\n\n\n{question}{anthropic.AI_PROMPT}"""
+
+    #client = anthropic.Client(api_key=openai_api_key)
+    #response = client.completions.create(
+        #prompt=prompt,
+        #stop_sequences=[anthropic.HUMAN_PROMPT],
+        #model="claude-v1", #"claude-2" for Claude 2 model
+        #max_tokens_to_sample=100,
+    #)
+    st.write("### Answer")
+    #st.write(response.completion)
